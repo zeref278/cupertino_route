5a6fc64e99ab5b644690e1ca67489cd406d35f75
Duy Truong <zeref278@gmail.com>
2025-04-17 18:12:14 +0700

    Add event bus support for swipe state synchronization and enhance README with new features

--- lib/cupertino_route.dart
+++ lib/cupertino_route.dart
@@ -1,3 +1,5 @@
 library cupertino_route;
 
 export 'package:cupertino_route/src/route/route.dart';
+export 'package:cupertino_route/src/route/event/event_bus.dart';
+export 'package:cupertino_route/src/theme/cupertino_route_theme.dart';
--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-const double kBackGestureWidth = 20.0;
-
-const double kMinFlingVelocity = 1.0; // Screen widths per second.
-
 // An eyeballed value for the maximum time it takes for a page to animate forward
 // if the user releases a page mid swipe.
 const int kMaxDroppedSwipePageForwardAnimationTime = 800; // Milliseconds.
@@ -24,32 +20,3 @@ const int kMaxPageBackAnimationTime = 300; // Milliseconds.
 ///
 /// A relatively rigorous eyeball estimation.
 const Color kCupertinoPageTransitionBarrierColor = Color(0x18000000);
-
-/// Barrier color for a Cupertino modal barrier.
-///
-/// Extracted from https://developer.apple.com/design/resources/.
-const Color kCupertinoModalBarrierColor = CupertinoDynamicColor.withBrightness(
-  color: Color(0x33000000),
-  darkColor: Color(0x7A000000),
-);
-
-// The duration of the transition used when a modal popup is shown.
-const Duration kModalPopupTransitionDuration = Duration(milliseconds: 335);
-
-// Offset from offscreen to the right to fully on screen.
-final Animatable<Offset> kRightMiddleTween = Tween<Offset>(
-  begin: const Offset(1.0, 0.0),
-  end: Offset.zero,
-);
-
-// Offset from fully on screen to 1/3 offscreen to the left.
-final Animatable<Offset> kMiddleLeftTween = Tween<Offset>(
-  begin: Offset.zero,
-  end: const Offset(-1.0 / 3.0, 0.0),
-);
-
-// Offset from offscreen below to fully on screen.
-final Animatable<Offset> kBottomUpTween = Tween<Offset>(
-  begin: const Offset(0.0, 1.0),
-  end: Offset.zero,
-);
